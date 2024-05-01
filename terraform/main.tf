7afeccd39855b22324a580429d24e1f0d1c0f6ba
nicolas.bertillod <nicolas.bertillod@iquall.net>
2024-05-01 16:52:32 -0300

    test aws action

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,28 @@
 terraform {
- required_providers {
-   aws = {
-     source = "hashicorp/aws"
-   }
- }
- 
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
 }
- 
+
 provider "aws" {
- region = "us-west-2"
+  region = "us-west-2"
 }
- 
+
 resource "aws_instance" "test_instance" {
- ami           = "ami-830c94e3"
- instance_type = "t2.nano"
- tags = {
-   Name = "test_instance"
- }
+  ami           = "ami-830c94e3"
+  instance_type = "t2.nano"
+  tags = {
+    Name = "test_instance"
+  }
 }
 
 resource "aws_instance" "test_instance_2" {
- ami           = "ami-830c94e3"
- instance_type = "t3.nano"
- tags = {
-   Name = "test_instance_2"
- }
+  ami           = "ami-830c94e3"
+  instance_type = "t3.nano"
+  tags = {
+    Name = "test_instance_2"
+  }
 }
