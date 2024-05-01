3616446f458925772eb70bd53bce53fc79217b13
nicolas.bertillod <nicolas.bertillod@iquall.net>
2024-05-01 17:19:48 -0300

    test aws action

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,3 +27,11 @@ resource "aws_instance" "test_instance_2" {
   }
 }
 
+resource "aws_instance" "test_instance_3" {
+  ami           = "ami-830c94e3"
+  instance_type = "t3.nano"
+  tags = {
+    Name = "test_instance_3"
+  }
+}
+
